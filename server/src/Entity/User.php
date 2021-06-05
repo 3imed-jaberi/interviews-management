3289a345b302375f4fcf402f366283b54da6e1c1
3imed-jaberi <imed_jebari@hotmail.fr>
2021-06-05 07:46:18 +0200

    add override tostring --easy-admin 🛹 ..

--- server/src/Entity/User.php
+++ server/src/Entity/User.php
@@ -402,4 +402,9 @@ class User implements UserInterface
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->firstname . '' . $this->lastname;
+    }
 }
