f2bf45055cd905cf2f8e258543cdf3e1f98a1464
3imed-jaberi <imed_jebari@hotmail.fr>
2021-06-19 18:21:28 +0200

    server updates --impr 🥪 ..

--- server/src/EventSubscriber/UserConfirmationSubscriber.php
+++ server/src/EventSubscriber/UserConfirmationSubscriber.php
@@ -1,56 +1,56 @@
 <?php
 
-namespace App\EventSubscriber;
+// namespace App\EventSubscriber;
 
-use ApiPlatform\Core\EventListener\EventPriorities;
-use App\Entity\UserConfirmation;
-use App\Security\UserConfirmationService;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-// use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
-use Symfony\Component\HttpKernel\Event\ViewEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
+// use ApiPlatform\Core\EventListener\EventPriorities;
+// use App\Entity\UserConfirmation;
+// use App\Security\UserConfirmationService;
+// use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+// use Symfony\Component\HttpFoundation\JsonResponse;
+// use Symfony\Component\HttpFoundation\Response;
+// // use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+// use Symfony\Component\HttpKernel\Event\ViewEvent;
+// use Symfony\Component\HttpKernel\KernelEvents;
 
-class UserConfirmationSubscriber implements EventSubscriberInterface
-{
-    /**
-     * @var UserConfirmationService
-     */
-    private $userConfirmationService;
+// class UserConfirmationSubscriber implements EventSubscriberInterface
+// {
+//     /**
+//      * @var UserConfirmationService
+//      */
+//     private $userConfirmationService;
 
-    public function __construct(
-        UserConfirmationService $userConfirmationService
-    ) {
-        $this->userConfirmationService = $userConfirmationService;
-    }
+//     public function __construct(
+//         UserConfirmationService $userConfirmationService
+//     ) {
+//         $this->userConfirmationService = $userConfirmationService;
+//     }
 
-    public static function getSubscribedEvents()
-    {
-        return [
-            KernelEvents::VIEW => [
-                'confirmUser',
-                EventPriorities::POST_VALIDATE,
-            ],
-        ];
-    }
+//     public static function getSubscribedEvents()
+//     {
+//         return [
+//             KernelEvents::VIEW => [
+//                 'confirmUser',
+//                 EventPriorities::POST_VALIDATE,
+//             ],
+//         ];
+//     }
 
-    public function confirmUser(ViewEvent $event)
-    {
-        $request = $event->getRequest();
+//     public function confirmUser(ViewEvent $event)
+//     {
+//         $request = $event->getRequest();
 
-        if ('api_user_confirmations_post_collection' !==
-            $request->get('_route')) {
-            return;
-        }
+//         if ('api_user_confirmations_post_collection' !==
+//             $request->get('_route')) {
+//             return;
+//         }
 
-        /** @var UserConfirmation $confirmationToken */
-        $confirmationToken = $event->getControllerResult();
+//         /** @var UserConfirmation $confirmationToken */
+//         $confirmationToken = $event->getControllerResult();
 
-        $this->userConfirmationService->confirmUser(
-            $confirmationToken->confirmationToken
-        );
+//         $this->userConfirmationService->confirmUser(
+//             $confirmationToken->confirmationToken
+//         );
 
-        $event->setResponse(new JsonResponse(null, Response::HTTP_OK));
-    }
-}
+//         $event->setResponse(new JsonResponse(null, Response::HTTP_OK));
+//     }
+// }
